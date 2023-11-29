43f59277651c5d7ada7dbca97cc5450eafa68440
MathItYT <benjamin.ubilla@uc.cl>
2023-11-28 22:40:30 -0300

    Reverted back to dill

--- src/manim_studio/saving_and_loading_mobjects.py
+++ src/manim_studio/saving_and_loading_mobjects.py
@@ -1,36 +1,12 @@
 from manim import Mobject
-import jsonpickle
-import jsonpickle.ext.numpy as jsonpickle_numpy
-
-jsonpickle_numpy.register_handlers()
-
-try:
-    import pandas as pd
-except ImportError:
-    pass
-else:
-    import jsonpickle.ext.pandas as jsonpickle_pandas
-    jsonpickle_pandas.register_handlers()
-
-try:
-    import gmpy2
-except ImportError:
-    pass
-else:
-    import jsonpickle.ext.gmpy as jsonpickle_gmpy
-    jsonpickle_gmpy.register_handlers()
+import dill as pickle
 
 
-def save_mobject(mobject: Mobject, file_name: str):
-    """Save a mobject to a file."""
-    with open(file_name, "w") as f:
-        json_str = jsonpickle.encode(mobject, unpicklable=False)
-        f.write(json_str)
+def load_mobject(path: str) -> Mobject:
+    with open(path, "rb") as f:
+        return pickle.load(f)
 
 
-def load_mobject(file_name: str) -> Mobject:
-    """Load a mobject from a file."""
-    with open(file_name, "r") as f:
-        json_str = f.read()
-        mobject = jsonpickle.decode(json_str)
-    return mobject
+def save_mobject(mobject: Mobject, path: str) -> None:
+    with open(path, "wb") as f:
+        pickle.dump(mobject, f)
