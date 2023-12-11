7485d7394354ab89de24cd2dd0ac3017042595b9
MathItYT <benjamin.ubilla@uc.cl>
2023-12-10 22:57:27 -0300

    Fixed import_from_file function

--- src/manim_studio/__main__.py
+++ src/manim_studio/__main__.py
@@ -16,7 +16,7 @@ def import_from_file(file_name):
     file_name = Path(file_name)
     if file_name.suffix != ".py":
         raise ValueError("File name must end with .py")
-    spec = importlib.util.spec_from_file_location(file_name, file_name)
+    spec = importlib.util.spec_from_file_location(file_name.stem, file_name)
     module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(module)
     return module
