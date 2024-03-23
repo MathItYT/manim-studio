da5360105b3803b7bf7b4c8ef9b199cfb351a791
MathItYT <benjamin.ubilla@uc.cl>
2024-03-23 10:16:55 -0300

    Modified test_startup.py

--- tests/test_startup.py
+++ tests/test_startup.py
@@ -1,7 +1,8 @@
 import subprocess
+import sys
 
 
 class TestStartup:
     def test_startup(self):
-        subprocess.run(["python3", "-m", "manim_studio",
+        subprocess.run([sys.executable, "-m", "manim_studio",
                        "--timeout", "5"], check=True)
